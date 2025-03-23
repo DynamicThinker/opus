337db13a042234ddbc89571fe4fef50bde45f458
Dynamo <pratish17124@gmail.com>
2025-03-23 18:18:25 +0530

    CC

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -62,7 +62,7 @@ const Paste = () => {
   }
 
   return (
-    <div className="bg-[#222629] min-h-screen py-8 px-8 pt-40 hide-scrollbar">
+    <div className="bg-[#222629] min-h-screen py-8 px-8 pt-30 hide-scrollbar">
       <div className="max-w-3xl mx-auto mb-5">
         <input
           type="text"
